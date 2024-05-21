9f609ffd9f84ae9b46fc15a1da48fded5830db61
Omar Hidalgo <omar_ehr@outlook.com>
2024-05-21 03:10:31 +0000

    FIXED lights logic

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,41 +1,39 @@
 import React, { useState } from "react";
 
 const TrafficLight = () => {
-  const [green, setGreen] = useState(true);
-  const [yellow, setYellow] = useState(false);
-  const [red, setRed] = useState(false);
+  const [color, setColor] = useState("");
 
   return (
     <>
       <div className="card mt-2 bg-dark mx-auto col-2 rounded}">
         <div class="card-body">
-          <div>
+          <div className="my-2">
             <button
-              className={`btn btn-danger btn-lg ${
-                !red && "opacity-25"
+              className={`btn btn-danger btn-lg p-5 ${
+                color != "red" && "opacity-25"
               } rounded-circle`}
               onClick={() => {
-                setRed(true), setYellow(false), setGreen(false);
+                setColor("red");
               }}
             ></button>
           </div>
-          <div>
+          <div className="my-2">
             <button
-              className={`btn btn-warning btn-lg ${
-                !yellow && "opacity-25"
+              className={`btn btn-warning btn-lg p-5 my-3 ${
+                color != "yellow" && "opacity-25"
               } rounded-circle`}
               onClick={() => {
-                setYellow(true), setRed(false), setGreen(false);
+                setColor("yellow");
               }}
             ></button>
           </div>
-          <div>
+          <div className="my-2">
             <button
-              className={`btn btn-success btn-lg ${
-                !green && "opacity-25"
+              className={`btn btn-success btn-lg p-5 ${
+                color != "green" && "opacity-25"
               } rounded-circle`}
               onClick={() => {
-                setGreen(true), setRed(false), setYellow(false);
+                setColor("green");
               }}
             ></button>
           </div>
@@ -44,14 +42,5 @@ const TrafficLight = () => {
     </>
   );
 };
-
-// <div class="card" style="width: 18rem;">
-
-//     Card title</h5>
-//     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//     <a href="#" class="card-link">Card link</a>
-//     <a href="#" class="card-link">Another link</a>
-//   </div>
 
 export default TrafficLight;
